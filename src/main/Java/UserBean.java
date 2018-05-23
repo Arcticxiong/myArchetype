508f2363cea05bf7a04898e7ce5480adbddd22de
Arcticxiong <407849770@qq.com>
2018-05-23 19:17:30 +0800

    1

---
+++ src/main/Java/UserBean.java
@@ -0,0 +1,20 @@
+/**
+ * @program: myArchetype
+ * @description: 用户实体
+ * @author: Arctic_Xiong
+ * @create: 2018-05-23 19:16
+ **/
+
+/**
+ * @program: myArchetype
+ *
+ * @description: 用户实体
+ *
+ * @author: Arctic_Xiong
+ *
+ * @create: 2018-05-23 19:16
+ **/
+public class UserBean {
+
+
+}
