0fbebf8d550e0ca35fe49531fb1f3c8d89af2888
Arcticxiong <407849770@qq.com>
2018-05-23 19:19:42 +0800

    1

--- src/main/Java/UserBean.java
+++ src/main/Java/UserBean.java
@@ -16,5 +16,22 @@
  **/
 public class UserBean {
 
+    private String name;
+    private int age;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
